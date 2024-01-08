7625029836ea82c8f6196bb757a2215c31ce8a5e
j-doka <jdoka42@gmail.com>
2024-01-08 12:56:50 +0000

    added md

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://jessedoka.co/sitemap.xml',
+    host: 'https://jessedoka.co',
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+import { getBlogPosts } from "@/app/db/blog"
+
+export default async function sitemap() {
+  let blogs = getBlogPosts().map((post) => ({
+    url: `https://jessdoka.co/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }));
+
+  let routes = ['', '/blog', '/work'].map((route) => ({
+    url: `https://jessedoka.co/${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }));
+
+  return [...routes, ...blogs];
+}
