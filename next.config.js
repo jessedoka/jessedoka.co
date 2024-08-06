a6b3e153f353092486b1a5d8dbaef07c69c71f33
Restyled.io <commits@restyled.io>
2024-08-06 23:38:32 +0000

    Restyled by clang-format

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 const MillionLint = require('@million/lint');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-        ],
-    },
-    reactStrictMode: true,
-    webpack: (config, options) => {
-        config.module.rules.push({
-            test: /\.(glsl|vs|fs|vert|frag)$/,
-            use: ['raw-loader', 'glslify-loader'],
-        });
+  images : {
+    remotePatterns : [
+      {
+        protocol : 'https',
+        hostname : 'images.unsplash.com',
+      },
+    ],
+  },
+  reactStrictMode : true,
+  webpack : (config, options) => {
+    config.module.rules.push({
+      test : /\.(glsl|vs|fs|vert|frag)$/,
+      use : [ 'raw-loader', 'glslify-loader' ],
+    });
 
-        return config;
-    }
+    return config;
+  }
 };
 module.exports = nextConfig;
